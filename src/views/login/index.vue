a4743e7dd267fb4035ad08ef93be8b451312c626
zhangyouxiu <zjb15239430906@gmail.com>
2023-07-30 23:35:47 +0800

    feat✨: 登录和注册表单校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -17,7 +17,7 @@
         </h2>
         <div class="mb-2 text-center text-gray-500">很高兴再次见到您!</div>
 
-        <a-form :model="loginData" layout="vertical">
+        <a-form :model="loginData" layout="vertical" :rules="formRules" @submit="submitLoginData">
           <a-form-item
             field="email"
             label="电子邮件"
@@ -26,7 +26,12 @@
             required
             asterisk-position="end"
           >
-            <a-input v-model="loginData.email" />
+            <a-input
+              v-model.trim="loginData.email"
+              allow-clear
+              placeholder="请输入电子邮件"
+              autocomplete="off"
+            />
           </a-form-item>
           <a-form-item
             field="password"
@@ -36,7 +41,13 @@
             required
             asterisk-position="end"
           >
-            <a-input v-model="loginData.password" />
+            <a-input-password
+              v-model.trim="loginData.password"
+              allow-clear
+              placeholder="请输入密码"
+              autocomplete="off"
+              @keyup.enter="submitLoginData"
+            />
             <template #extra>
               <span
                 class="cursor-pointer text-sm font-semibold text-violet-600 hover:text-violet-500"
@@ -45,13 +56,13 @@
             </template>
           </a-form-item>
           <a-form-item>
-            <a-button type="primary" long>登录</a-button>
+            <a-button type="primary" long html-type="submit">登录</a-button>
             <template #extra>
               <p class="text-center text-sm text-gray-500">
                 没有账号?
                 <span
                   class="cursor-pointer font-semibold leading-6 text-violet-600 hover:text-violet-500"
-                  @click="goRegister"
+                  @click="goLoginOrRegister"
                   >立即注册</span
                 >
               </p>
@@ -63,7 +74,7 @@
       <div
         class="box-border w-[240px] h-full p-2 hidden lg:flex flex-col justify-center items-center"
       >
-        <canvas class="w-44 h-44" ref="loginCode"></canvas>
+        <canvas class="w-44 h-44" ref="loginQRCode"></canvas>
         <h2 class="mt-4 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           通过二维码登录
         </h2>
@@ -85,7 +96,12 @@
         创建一个账号
       </h2>
 
-      <a-form :model="registerData" layout="vertical">
+      <a-form
+        :model="registerData"
+        layout="vertical"
+        :rules="formRules"
+        @submit="submitRegisterData"
+      >
         <a-form-item
           field="email"
           label="电子邮件"
@@ -94,10 +110,27 @@
           required
           asterisk-position="end"
         >
-          <a-input v-model="registerData.email" />
+          <a-input
+            v-model.trim="registerData.email"
+            allow-clear
+            placeholder="请输入电子邮件"
+            autocomplete="off"
+          />
         </a-form-item>
-        <a-form-item field="nickname" label="昵称" validate-trigger="change" feedback>
-          <a-input v-model="registerData.nickname" />
+        <a-form-item
+          field="nickname"
+          label="昵称"
+          validate-trigger="change"
+          feedback
+          required
+          asterisk-position="end"
+        >
+          <a-input
+            v-model.trim="registerData.nickname"
+            allow-clear
+            placeholder="请输入昵称"
+            autocomplete="off"
+          />
         </a-form-item>
         <a-form-item
           field="username"
@@ -107,7 +140,12 @@
           required
           asterisk-position="end"
         >
-          <a-input v-model="registerData.username" />
+          <a-input
+            v-model.trim="registerData.username"
+            allow-clear
+            placeholder="请输入用户名"
+            autocomplete="off"
+          />
         </a-form-item>
         <a-form-item
           field="password"
@@ -117,10 +155,16 @@
           required
           asterisk-position="end"
         >
-          <a-input v-model="registerData.password" />
+          <a-input-password
+            v-model.trim="registerData.password"
+            allow-clear
+            placeholder="请输入密码"
+            autocomplete="off"
+            @keyup.enter="submitRegisterData"
+          />
         </a-form-item>
         <a-form-item>
-          <a-button type="primary" long>注册</a-button>
+          <a-button type="primary" long html-type="submit">注册</a-button>
           <template #extra>
             <p class="text-center text-sm text-gray-500">
               进行注册即表示您同意藏猫猫的<a-link href="#">服务条款</a-link>和<a-link href="#"
@@ -131,7 +175,7 @@
         </a-form-item>
         <div
           class="text-sm cursor-pointer font-semibold leading-6 text-violet-600 hover:text-violet-500"
-          @click="goRegister"
+          @click="goLoginOrRegister"
         >
           已经拥有账号?
         </div>
@@ -141,15 +185,66 @@
 </template>
 
 <script setup>
-import { onMounted, ref, reactive } from 'vue'
+import { onMounted, ref, reactive, getCurrentInstance } from 'vue'
 import QRCode from 'qrcode'
 
+const formRules = {
+  email: [
+    { required: true, message: '电子邮件 - 这是必填项🫡' },
+    {
+      validator: (value, cb) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+        !emailRegex.test(value) && cb('当前输入电子邮件不符合规则🤔')
+      }
+    }
+  ],
+  password: [
+    { required: true, message: '密码 - 这是必填项🫡' },
+    {
+      minLength: 8,
+      message: '密码最小长度为 8 个字符，要求包含至少一个大写字母、一个小写字母和一个数字🫡'
+    },
+    {
+      validator: (value, cb) => {
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/
+        !passwordRegex.test(value) && cb('当前输入密码不符合规则🤔')
+      }
+    }
+  ],
+  nickname: [
+    { required: true, message: '昵称 - 这是必填项🫡' },
+    {
+      minLength: 4,
+      message: '昵称只能包含字母、数字和下划线，且长度在 4 到 16 个字符之间🫡'
+    },
+    {
+      validator: (value, cb) => {
+        const nicknameRegex = /^[a-zA-Z0-9_]{4,16}$/
+        !nicknameRegex.test(value) && cb('当前输入昵称不符合规则🤔')
+      }
+    }
+  ],
+  username: [
+    { required: true, message: '用户名 - 这是必填项🫡' },
+    {
+      minLength: 4,
+      message: '用户名只能包含字母、数字和下划线，且长度在 4 到 16 个字符之间🫡'
+    },
+    {
+      validator: (value, cb) => {
+        const usernameRegex = /^[a-zA-Z0-9_]{4,16}$/
+        !usernameRegex.test(value) && cb('当前输入用户名不符合规则🤔')
+      }
+    }
+  ]
+}
+
 const loginOrRegister = ref('login')
+const loginQRCode = ref(null)
 const loginData = reactive({
   email: '',
   password: ''
 })
-const loginCode = ref(null)
 const registerData = reactive({
   email: '',
   nickname: '',
@@ -157,17 +252,27 @@ const registerData = reactive({
   password: ''
 })
 
-const generateQRCode = () => {
+const generateLoginQRCode = () => {
   const codeData = '暂不支持二维码登录功能'
-  QRCode.toCanvas(loginCode.value, codeData, (error) => {
+  QRCode.toCanvas(loginQRCode.value, codeData, (error) => {
     if (error) {
-      // console.error('生成二维码失败：', error);
+      getCurrentInstance().appContext.config.globalProperties.$message.error(
+        `生成登录二维码失败: ${error}`
+      )
     }
   })
 }
-const goRegister = () => {
+const goLoginOrRegister = () => {
   loginOrRegister.value = loginOrRegister.value === 'login' ? 'register' : 'login'
 }
+const submitLoginData = ({ values, errors }) => {
+  if (!errors) {
+  }
+}
+const submitRegisterData = ({ values, errors }) => {
+  if (!errors) {
+  }
+}
 
-onMounted(() => generateQRCode())
+onMounted(() => generateLoginQRCode())
 </script>
